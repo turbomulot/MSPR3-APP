d1cbea372ce5236795d2650a31b2880ec1540b37
¨turbomulot¨ <rcdc@randomcritd.fr>
2026-06-18 10:12:27 +0200

    project start

---
+++ mspr3/src-tauri/gen/android/app/src/main/java/com/parceval/mspr3/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.parceval.mspr3
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
